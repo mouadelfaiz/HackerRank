7b6b015b04773eaea73417c7ea5fa801bc84314b
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-14 21:19:59 -0400

    Update index.js

--- coding challanges/HackerRank/index.js
+++ coding challanges/HackerRank/index.js
@@ -1,1 +1,24 @@
-// this is my HackerRank solutions
+// this is my HackerRank solutions:
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function compareTriplets(a, b) {
+  let bobScore = 0;
+    let aliceScore = 0;
+  
+    for (let i = 0; i < 3; i++) {
+      if (a[i] > b[i]) {
+        aliceScore++;
+      } else if (b[i] > a[i]) {
+        bobScore++;
+      }
+    }
+    const resultArray = [aliceScore, bobScore];
+    return resultArray;
+  }
