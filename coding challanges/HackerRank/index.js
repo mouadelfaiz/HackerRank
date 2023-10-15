fc85e1d2e3ac10528bcb3659d71a91d14642e3fc
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-15 17:37:37 -0400

    adding some comments
    highlights the mistakes that I did

--- coding challanges/HackerRank/index.js
+++ coding challanges/HackerRank/index.js
@@ -1,4 +1,5 @@
 // this is my HackerRank solutions:
+//----------------- solution 1 --------------------//
 /*
  * Complete the 'compareTriplets' function below.
  *
@@ -8,17 +9,25 @@
  *  2. INTEGER_ARRAY b
  */
 
+
 function compareTriplets(a, b) {
   let bobScore = 0;
-    let aliceScore = 0;
-  
-    for (let i = 0; i < 3; i++) {
-      if (a[i] > b[i]) {
-        aliceScore++;
-      } else if (b[i] > a[i]) {
-        bobScore++;
-      }
+  let aliceScore = 0;
+
+  for (let i = 0; i < 3; i++) {
+    if (a[i] > b[i]) {
+      aliceScore++;
+    } else if (b[i] > a[i]) {
+      bobScore++;
     }
-    const resultArray = [aliceScore, bobScore];
-    return resultArray;
   }
+  const resultArray = [aliceScore, bobScore];
+  return resultArray;
+}
+
+// mistakes:
+// 1-Incorrect initialization initializing bobScore and aliceScore inside the loop
+// 2-incorrect usage of return, using the return inside the loop and using return multiple time, which will exit the functin on the first iteration, i should use the varaibles to accumulate the scores and then return the result after the loop is done.
+
+//------------------ solution 2--------------------//
+
